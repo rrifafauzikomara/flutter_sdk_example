bdcc24f4bddb5e25be6cccc7bd5a6f3fd7f9bb4d
rrifafauzikomara <rifafauzi6@gmail.com>
2023-05-06 04:23:34 +0700

    feat: add wrapping class unit testing

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,58 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_gen/gen_l10n/phrase_localizations.dart';
+import 'package:flutter_sdk_example/localization/example_localization.dart';
 import 'package:flutter_sdk_example/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('using context', (tester) async {
-    late BuildContext testContext;
-    await tester.pumpWidget(MaterialApp(
-      localizationsDelegates: PhraseLocalizations.localizationsDelegates,
-      supportedLocales: PhraseLocalizations.supportedLocales,
-      home: Builder(builder: (BuildContext context) {
-        // use a Builder widget so that we can get a build context element in a tree branch with translations setup
-        testContext = context;
-        return MyHomePage(onChangeLocale: (_) {});
-      }),
-    ));
-
-    await tester.pumpAndSettle();
-
-    expect(
-      find.text(AppLocalizations.of(testContext).textSimple),
-      findsOneWidget,
-    );
-    expect(
-      find.text(AppLocalizations.of(testContext).textWithParams(123456, "(!)")),
-      findsOneWidget,
-    );
-    expect(
-      find.text(AppLocalizations.of(testContext)
-          .textWithDate(DateTime.utc(1996, 7, 10))),
-      findsOneWidget,
-    );
-    expect(
-      find.text(AppLocalizations.of(testContext).textPlural(0)),
-      findsOneWidget,
-    );
-
-    final fab = find.widgetWithIcon(FloatingActionButton, Icons.add);
-    await tester.tap(fab);
-    await tester.pump();
-
-    expect(find.text(AppLocalizations.of(testContext).textPlural(1)),
-        findsOneWidget);
-  });
-
   testWidgets('using AppLocalizations', (tester) async {
-    late AppLocalizations localizations;
+    late ExampleLocalization localizations;
     await tester.pumpWidget(MaterialApp(
       localizationsDelegates: PhraseLocalizations.localizationsDelegates,
       supportedLocales: PhraseLocalizations.supportedLocales,
       home: Builder(builder: (BuildContext context) {
         // use a Builder widget so that we can get a build context element in a tree branch with translations setup
-        localizations = AppLocalizations.of(context);
+        localizations = ExampleLocalization(context: context);
         return MyHomePage(onChangeLocale: (_) {});
       }),
     ));
@@ -60,19 +20,19 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(
-      find.text(localizations.textSimple),
+      find.text(localizations.tr.textSimple),
       findsOneWidget,
     );
     expect(
-      find.text(localizations.textWithParams(123456, "(!)")),
+      find.text(localizations.tr.textWithParams(123456, "(!)")),
       findsOneWidget,
     );
     expect(
-      find.text(localizations.textWithDate(DateTime.utc(1996, 7, 10))),
+      find.text(localizations.tr.textWithDate(DateTime.utc(1996, 7, 10))),
       findsOneWidget,
     );
     expect(
-      find.text(localizations.textPlural(0)),
+      find.text(localizations.tr.textPlural(0)),
       findsOneWidget,
     );
 
@@ -80,6 +40,6 @@ void main() {
     await tester.tap(fab);
     await tester.pump();
 
-    expect(find.text(localizations.textPlural(1)), findsOneWidget);
+    expect(find.text(localizations.tr.textPlural(1)), findsOneWidget);
   });
 }
