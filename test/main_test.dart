12fdbe609872397c6fb926a2e7baaa09589a3e8b
rrifafauzikomara <rifafauzi6@gmail.com>
2023-05-06 04:45:17 +0700

    feat: update unit testing for localization from get it

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/phrase_localizations.dart';
+import 'package:flutter_sdk_example/di/dependency.dart';
 import 'package:flutter_sdk_example/localization/example_localization.dart';
 import 'package:flutter_sdk_example/main.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,15 +8,18 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   late ExampleLocalization localizations;
 
+  setUpAll(() async {
+    sl.registerLazySingleton(
+        () => ExampleLocalization(context: navigatorKey.currentContext!));
+    localizations = sl<ExampleLocalization>();
+  });
+
   testWidgets('using AppLocalizations', (tester) async {
     await tester.pumpWidget(MaterialApp(
       localizationsDelegates: PhraseLocalizations.localizationsDelegates,
       supportedLocales: PhraseLocalizations.supportedLocales,
-      home: Builder(builder: (BuildContext context) {
-        // use a Builder widget so that we can get a build context element in a tree branch with translations setup
-        localizations = ExampleLocalization(context: context);
-        return MyHomePage(onChangeLocale: (_) {});
-      }),
+      home: MyHomePage(onChangeLocale: (_) {}),
+      navigatorKey: navigatorKey,
     ));
 
     await tester.pumpAndSettle();
